e13c311b43ed282008cf2416fa62fd6b29fd4c94
shichiye <shichiye@126.com>
2022-06-03 17:49:08 +0800

    feat: 集成element plus图标和nanoid

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/index.css'
 
-createApp(App).mount('#app')
+import * as ELIcons from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+for (const name in ELIcons) {
+  app.component(name, (ELIcons as any)[name])
+}
+
+app.mount('#app')
